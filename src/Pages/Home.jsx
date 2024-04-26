0f5da882e27d8a10b23b8b3cc3008d0afff953d8
pala72748 <pala72748@gmail.com>
2024-04-26 22:50:05 +0530

    pagination

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Components/Header/Header';
 import Breadcrumb from '../Components/Breadcrumb/Breadcrumb';
 import Sidebar from '../Components/Sidebar/Sidebar';
+import Pagination from '../Components/Pagination/Pagination';
 
 
 export const Home = () => {
@@ -10,12 +11,16 @@ export const Home = () => {
             <Header />
             <Breadcrumb label={"All European Brand"} href={"/home"} />
             <div className='container mx-auto flex'>
-                <div className='w-3/12 border'>
+                <div className='w-3/12'>
                     <Sidebar />
                 </div>
-                <div>
-                    <h2 cl>All European brands</h2>
+                <div className='w-9/12 mt-8'>
+                    <h2 className='text-2xl'>All European brands</h2>
+                    <p>Shop independent brands from around the world at wholesale prices.</p>
                 </div>
+            </div>
+            <div className='border-y'>
+                <Pagination />
             </div>
         </>
     )
